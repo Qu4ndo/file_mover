cdb7d49e22713540b45388f911fb1e0f46874dd4
Manuel <pfandlmanuel@protonmail.com>
2020-08-24 20:01:42 +0200

    get basic structure

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+# Check what is in the dir
+arr = os.listdir()
+print(arr)
+
+# open checking file (json?)
+# look for new files (for loop)
+# if new file - move to second folder and get timestamp for new index (json file)
+# remove all old files from directory (for loop)
