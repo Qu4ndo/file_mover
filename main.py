abf2bb698acc794a55fae166802531d581326c7b
Manuel <pfandlmanuel@protonmail.com>
2020-09-01 20:51:34 +0200

    testing things out

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
 
 # Check what is in the dir
-arr = os.listdir()
+# listdir need a variable path -> current = os.getcwd()
+arr = os.listdir(os.getcwd())
 print(arr)
+
+for ar in arr:
+    print(ar)
 
 # open checking file (json?)
 # look for new files (for loop)
 # if new file - move to second folder and get timestamp for new index (json file)
 # remove all old files from directory (for loop)
+
+
+#test dumping to json with timestamp
