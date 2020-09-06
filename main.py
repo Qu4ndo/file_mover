f246d6920deb7e6b8ad324b7631f6b9de166cd0c
Manuel <pfandlmanuel@protonmail.com>
2020-09-06 16:15:43 +0200

    Added opening and closing of data.json file

--- main.py
+++ main.py
@@ -2,24 +2,34 @@ import os
 import time
 import json
 
+#ignore following items (modify if needed)
+remove_list = ['LICENSE', 'main.py', '.git', 'data.json', 'README.md']
+
 # get time tick
 tick = int(time.time())
 
-# Check what is in the dir
-# listdir need a variable path -> current = os.getcwd()
+# check what is in the dir
+    # listdir need a variable path -> current = os.getcwd()
 print("Output List:")
 array = os.listdir(os.getcwd())
+array = list(set(array).difference(set(remove_list)))
 print(array)
 
+
 # open data.json file
-# scan if new files appeared
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+    # scan if new files appeared
+    print("\nData opened from json file")
+    for items in data:
+        print(items)
+
+
 # move new files
 # remove old files (outdated)
-# save new files to data.json
 
-
-
-#test dumping to json with timestamp
+#adding timestamp to dict
 print("\nOutput List inkl. Timestamps:")
 
 output = dict()
@@ -27,9 +37,17 @@ output = dict()
 for item in array:
     output[item] = tick
 
-print output
+print(output)
 
-
+"""
+#maybe not needed
 # test convert output to json
 json_string = json.dumps(output)
 print(json_string)
+"""
+
+
+# save new files to data.json
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(output, f, ensure_ascii=False, indent=4)
+print("\nData dumped to json file")
