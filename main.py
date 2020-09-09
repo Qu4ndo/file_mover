c00aec4a50d6d1545340df57a8c97dc9befad74c
Manuel Pfandl <55713049+Qu4ndo@users.noreply.github.com>
2020-09-09 09:47:49 +0200

    Edited possible error

--- main.py
+++ main.py
@@ -72,6 +72,6 @@ for item in item_not_in_data:
 data.update(output)
 
 # save new files to data.json
-with open("data.json", "w", encoding="utf-8") as f:
+with open("data.json", "w") as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 print("\nJob finished! Ready for a new run?")
